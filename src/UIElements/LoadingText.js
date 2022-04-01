3afc24da13e1b19bbb613bed9b47b9bc2fa3046c
Max <maximilianopalomeque@gmail.com>
2022-04-01 20:06:51 -0300

    loading animation

--- src/UIElements/LoadingText.js
+++ src/UIElements/LoadingText.js
@@ -2,6 +2,8 @@ import { React } from "react";
 import Container from "@mui/material/Container";
 import Grid from "@mui/material/Grid";
 
+import walkingAnimation from "../images/pokemon-walking-animation.gif";
+
 import "animate.css";
 
 const LoadingText = () => {
@@ -16,6 +18,16 @@ const LoadingText = () => {
             >
               {"loading"}
             </h1>
+          </Grid>
+          <Grid container xl={12} s={12} mt={0} justifyContent="center" mb={2}>
+            <Grid item>
+              <img
+                src={walkingAnimation}
+                alt="pikachu running"
+                width={70}
+                height={40}
+              ></img>
+            </Grid>
           </Grid>
         </Grid>
       </Container>
