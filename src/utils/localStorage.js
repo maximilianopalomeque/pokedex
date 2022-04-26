5d6e5c3dc5747705715ec668c5a00aa7a42c12e1
Max <maximilianopalomeque@gmail.com>
2022-04-26 07:19:24 -0300

    folder structure

--- src/utils/localStorage.js
+++
@@ -1,8 +0,0 @@
-export const saveInLocalStorage = (data) => {
-  localStorage.setItem("pokemon-data", JSON.stringify(data));
-};
-
-export const getFromLocalStorage = () => {
-  const data = JSON.parse(localStorage.getItem("pokemon-data"));
-  return data;
-};
